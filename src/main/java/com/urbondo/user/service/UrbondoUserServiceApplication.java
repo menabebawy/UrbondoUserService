43dddc962160d1f808f963e6b1ad3534b0e523de
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-05-21 18:02:20 +0200

    create an empty Gradle project

---
+++ src/main/java/com/urbondo/user/service/UrbondoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.urbondo.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbondoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbondoUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbondo/user/service/UrbondoUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbondo.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbondoUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
